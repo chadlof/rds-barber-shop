20c4c0afab046405c79ee942279e8ed62c87de86
DESKTOP-9JLHQB1\chadl <chadlof@gmail.com>
2018-04-28 06:08:47 -0500

    realigned storefront

--- src/components/pages/Home/components/StorefrontPic/StorefrontPic.js
+++ src/components/pages/Home/components/StorefrontPic/StorefrontPic.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types'
 import '../../../../../static/css/styles.css'
-import Img from 'react-image'
+// import Img from 'react-image'
 import storeFrontPic from '../../../../../static/images/storeFront.png'
 
 
 class StorefrontPic extends Component {
     render() {
       return (     
-        <div className="StorefrontPic">
-            <Img  
+        // <div className="StorefrontPic">
+            <img  className="StorefrontPic"
                 src={storeFrontPic} 
                 alt=""
-                width="100%"
+                // width="400px"
             
             />
-        </div>     
+        // </div>      
             
       );
     }
